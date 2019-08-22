71c7e39a7bc610317b336f225d1dbf44644b8b6e
Joris Roels <jorisro@cn1385.irc.ugent.be>
2019-08-22 13:17:18 +0200

    update

--- test.py
+++ test.py
@@ -1,36 +1,23 @@
-import matplotlib.pyplot as plt
 import imageio
 import torch
 
 from augmentation import FlipX, FlipY, Rotate90, AddNoise, RandomDeformation
 
-x = imageio.imread('elaine.png').astype('float')
+x = imageio.imread('img/elaine.png').astype('float')
 x = torch.Tensor(x).unsqueeze(0).unsqueeze(0).cuda()
-
-plt.imshow(x[0, 0, ...].cpu().numpy(), cmap='gray')
-plt.show()
 
 flipx = FlipX(x.size())
 y = flipx(x)
-plt.imshow(y[0, 0, ...].cpu().numpy(), cmap='gray')
-plt.show()
 
 flipy = FlipY(x.size())
 y = flipy(x)
-plt.imshow(y[0, 0, ...].cpu().numpy(), cmap='gray')
-plt.show()
 
 rotate = Rotate90()
 y = rotate(x)
-plt.imshow(y[0, 0, ...].cpu().numpy(), cmap='gray')
-plt.show()
+imageio.imwrite('elaine_rot.png', y[0, 0, ...].cpu().numpy())
 
 noise = AddNoise(sigma_min=20, sigma_max=20)
 y = noise(x)
-plt.imshow(y[0, 0, ...].cpu().numpy(), cmap='gray')
-plt.show()
 
 deform = RandomDeformation(x.size(), points=x.size(2) // 64, sigma=0.01)
 y = deform(x)
-plt.imshow(y[0, 0, ...].cpu().numpy(), cmap='gray')
-plt.show()
